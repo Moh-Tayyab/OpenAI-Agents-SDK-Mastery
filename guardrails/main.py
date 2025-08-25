3571c82087080299fd2450ceeaddb96101295757
Moh-Tayyab <m.tayyab1263@gmail.com>
2025-08-26 00:33:10 +0500

    updation

--- guardrails/main.py
+++ guardrails/main.py
@@ -1,4 +1,18 @@
-def main():
+from agent import Agent, Runner, PGuardrails
+
+async def main():
+    agent = Agent(
+        name = "Guardrails",
+        instructions = "You are a helpful assistant that follows guardrails.",
+        model = "gpt-4o-mini",
+        guardrail = Guardrails()
+    )
+    
+    result = await Runner.run(
+        starting_agent = agent,
+        input = "Hello, how are you?"
+        
+    )
     print("Hello from guardarails!")
 
 
