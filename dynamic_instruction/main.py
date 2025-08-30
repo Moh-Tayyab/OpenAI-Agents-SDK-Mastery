60030d2366b9a39497e88144a0d53584a598d9da
Moh-Tayyab <m.tayyab1263@gmail.com>
2025-08-30 19:49:45 +0500

    Refactor code structure for improved readability and maintainability

---
+++ dynamic_instruction/main.py
@@ -0,0 +1,48 @@
+from agents import Agent, Runner, RunContextWrapper, AsyncOpenAI, OpenAIChatCompletionsModel
+import asyncio
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+async def main():
+    MODEL_NAME = "gemini-2.0-flash"
+    API_KEY = os.getenv("GEMINI_API_KEY")
+    if not API_KEY:
+        raise ValueError("Gemini Api key not found.")
+    client = AsyncOpenAI(
+        api_key=API_KEY, 
+        base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+    )
+    model = OpenAIChatCompletionsModel(model=MODEL_NAME, openai_client=client)
+    # API_KEY = os.getenv("OPENAI_API_KEY")
+    # if not API_KEY:
+    #     raise ValueError("OpenAI Api key not found.")
+    
+    def my_dynamic_instruction(context: RunContextWrapper, agent: Agent) -> str:
+        # Direct string input access
+        user_text = str(getattr(context, "input", []))
+        if "beginner" in user_text:
+            return f"You are {agent.name}, explain Python like I'm a complete beginner."
+        elif "advanced" in user_text:
+            return f"You are {agent.name}, explain with advanced technical details."
+        else:
+            return f"You are {agent.name}, explain Python programming clearly and concisely."
+
+     
+    agent=Agent(
+        name="Python Helper",
+        instructions=my_dynamic_instruction,
+        model=model,
+        #model_settings=ModelSettings(temparture=0.1, max_tokens=100)
+    )  
+    
+    querry= input("Ask me anything about python programming: ")
+    result= await Runner.run(
+        starting_agent=agent, 
+        input= querry
+    )
+    print(result.final_output)
+    
+if __name__ == "__main__":
+   asyncio.run(main())
