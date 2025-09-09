1269ba764ff8b96cdfd1860d9235beb2bee3e6d2
Moh-Tayyab <m.tayyab1263@gmail.com>
2025-09-10 02:14:09 +0500

    update agent definitions for improved clarity and error handling

--- guardrails/main.py
+++ guardrails/main.py
@@ -6,60 +6,89 @@ from agents import (
     RunContextWrapper,
     TResponseInputItem,
     input_guardrail,
-)
-
-class bank_info(BaseModel):
-    account_num: str
-    rout_num: int
-    amount: float
-
-# Global bank agent definition
-bank_agent = Agent(
-    name="bank_info_agent",
-    instructions="You are a banking agent. You will collect bank information from users.",
-    model="gpt-4o-mini",
-    output_type=bank_info
-)
-
-@input_guardrail
-async def bank_guardrail(
-    ctx: RunContextWrapper[None],
-    agent: Agent,
-    input: str | list[TResponseInputItem]
-) -> GuardrailFunctionOutput:
+    AsyncOpenAI,
+    OpenAIChatCompletionsModel
     
-    result = await Runner.run(
-        bank_agent,
-        input,
-        context=ctx.context
+)
+from agents.run import RunConfig
+from dotenv import load_dotenv
+import os
+import asyncio
+load_dotenv()
+
+async def main():
+    gemini_key = os.getenv("GEMINI_API_KEY")
+    if not gemini_key:
+        raise ValueError("api key not found.")
+    
+    client=AsyncOpenAI(
+        api_key=gemini_key,
+        base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
     )
     
-    return GuardrailFunctionOutput(
-        output_info=result.final_output,
-        tripwire_triggered=result.final_output.account_num
-    )
-
-# Customer service agent with guardrail
-customer_service_agent = Agent(
-    name="customer_service_agent",
-    instructions="You are a customer service agent. You will assist users with their banking issues.",
-    model="gpt-4o-mini",
-    input_guardrails=[bank_guardrail]
-)  
-
-# Test function
-async def test_bank_info():
-    try:
-        result = await Runner.run(
-            customer_service_agent,
-            "Help me with my bank account. My account number is 000000000 and routing number is 123456789. I want to transfer $1000."
+    model=OpenAIChatCompletionsModel(model="gemini-2.0-flash", openai_client=client)
+    
+    config=RunConfig(
+        model=model,
+        model_provider=client
         )
-        print("Guardrail failed - this should not print")
+    
+
+    class bank_info(BaseModel):
+        account_num: str
+        rout_num: int
+        amount: float
+
+    # Global bank agent definition
+    bank_agent = Agent(
+        name="bank_info_agent",
+        instructions="You are a banking agent. You will collect bank information from users.",
+        model="gpt-4o-mini",
+        output_type=bank_info
+    )
+
+    @input_guardrail
+    async def bank_guardrail(
+        ctx: RunContextWrapper[None],
+        bank_agent: Agent,
+        input: str | list[TResponseInputItem]
+    ) -> GuardrailFunctionOutput:
+        
+        result = await Runner.run(
+            bank_agent,
+            input,
+            context=ctx.context
+        )
+        
+        return GuardrailFunctionOutput(
+            output_info=result.final_output,
+            tripwire_triggered=result.final_output.account_num
+        )
+
+    # Customer service agent with guardrail
+    customer_service_agent = Agent(
+        name="customer_service_agent",
+        instructions="You are a customer service agent. You will assist users with their banking issues.",
+        model="gpt-4o-mini",
+        input_guardrails=[bank_guardrail]
+    )  
+
+
+    try:
+            result = await Runner.run(
+                starting_agent = customer_service_agent,
+            input= "Help me with my bank account. My account number is 000000000 and routing number is 123456789. I want to transfer $1000.",
+            run_config = config
+            )
+            print("Guardrail failed - this should not print")
     except InputGuardrailTripwireTriggered as e:
-     print("Tripwire triggered:", e)
-     print("Guardrail output:", e.guardrail_output.output_info)
-     
-     
-     
-     
-     
+            print("Tripwire triggered:", e)
+            print("Guardrail output:", e.guardrail_output.output_info)
+        
+    print(result.final_output)
+    
+if __name__ == "__main__":
+    asyncio.run(main())    
+    
+    
+    
--- handoffs/main.py
+++ handoffs/main.py
@@ -1,23 +1,34 @@
-from agents import Runner, Agent
+from agents import Runner, Agent, OpenAIChatCompletionsModel, AsyncOpenAI
 import asyncio
 import os
 from dotenv import load_dotenv
-import chainlit as cl
+#import chainlit as cl
 load_dotenv()
 
-@cl.on_message
-async def main(message: cl.message):
-    API_KEY = os.getenv("OPENAI_API_KEY")
-    if not API_KEY:
-        raise ValueError("OpenAI Api key not found.")
+#@cl.on_message
+async def main():
+    # API_KEY = os.getenv("OPENAI_API_KEY")
+    # if not API_KEY:
+    #     raise ValueError("OpenAI Api key not found.")
+    gemini_key = os.getenv("GEMINI_API_KEY")
+    if not gemini_key:
+        raise ValueError("api key not found.")
     
+    client=AsyncOpenAI(
+        api_key=gemini_key,
+        base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+    )
+    
+    model=OpenAIChatCompletionsModel(model="gemini-2.0-flash", openai_client=client)
     booking_agent = Agent(
         name = "Booking Agent",
-        instructions = "Handle booking inquiries"
+        instructions = "Handle booking inquiries",
+        model=model
     )
     refund_agent = Agent(
         name = "Refund Agent",
-        instructions = "process refund inquiries"
+        instructions = "process refund inquiries",
+        model=model
     )
     
     tri_agent = Agent(
@@ -25,16 +36,17 @@ async def main(message: cl.message):
         instructions =  "You are a helpdesk agent."
         "If the user asks about booking, hand off to the Booking agent."
         "If about refunds, hand off to the Refund agent.",
-        model = "gpt-4o-mini",
-        handoffs = [booking_agent, refund_agent]
+        model = model,
+        handoffs=[booking_agent, refund_agent]
     )
-    
+    query = input("user query: ")
     result = await Runner.run(
         starting_agent = tri_agent,
-        input = message.content
+        #input = message.content
+        input = query
     )
-    await cl.Message(content=result.final_output).send()
-    #print(result.final_output)
+    #await cl.Message(content=result.final_output).send()
+    print(result.final_output)
 
 
 if __name__ == "__main__":
