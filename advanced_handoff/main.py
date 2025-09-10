cc230fdbb79f7b5aedde617141fb007ee4da39d6
Moh-Tayyab <m.tayyab1263@gmail.com>
2025-09-10 16:29:17 +0500

    updation

--- advanced_handoff/main.py
+++ advanced_handoff/main.py
@@ -1,4 +1,4 @@
-#Customizing handoffs via the handoff() function
+#Customizing via handoff() function
 from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel, function_tool, handoff, RunContextWrapper
 from dotenv import load_dotenv
 from agents.run import RunConfig
@@ -34,26 +34,24 @@ async def main():
     # def route_query(query):
     #     if "linkedin" in query.lower():
     #     #yh aider custom handoff ka concept use ho rha hai
-    #       return handoff(name = "LinkedIn Post Assistant", instructions = "You are a helpfull assitant that help to post content on linkedin.")
+    #       return handoff(name = "LinkedIn Assistant", instructions = "You are a helpfull assitant that help to post content on linkedin.")
     #     elif "whatsapp" in query.lower():
     #         return handoff(name = "Whatsapp Assistant", instructions = "You are a helpfull assistant to handle my personal whatsapp",)
     #     else:
     #         return "I can’t find a suitable assistant."
         
-    linkedin_agent = Agent(
-        name = "LinkedIn Post Assistant",
-        instructions = "You are a helpfull assitant that help to post content on linkedin.",
-        model=model
-        
-    )
+        #basic hands off
+    # linkedin_agent = Agent(
+    #     name = "LinkedIn Assistant",
+    #     instructions = "You only solve query about linkedin."
+    # )
     
-    whatsapp_agent = Agent(
-        name = "Whatsapp Assistant",
-        instructions = "You are a helpfull assistant to handle my personal whatsapp",
-        model=model
-    )
+    # whatsapp_agent = Agent(
+    #     name = "Whatsapp Assistant",
+    #     instructions = "You only solve query about whatsapp"
+    # )
     
-    def on_handoff(ctx: RunContextWrapper[None], agent: Agent):
+    def on_handoff(agent: Agent, ctx: RunContextWrapper[None]):
         agent_name = agent.name
         print("*"*10)
         print(f"Handoff agent name: {agent_name}")
@@ -61,10 +59,14 @@ async def main():
     
     triage_agent = Agent(
         name="Manager Assistant",
-        instructions = "you can help user smoothly and deliery user query to specialist Assistant.",
-        model=model,
+        instructions = ("""
+        You are a manager. You must never answer directly.
+        If query is about LinkedIn → delegate to LinkedIn Assistant.
+        If query is about WhatsApp → delegate to WhatsApp Assistant.
+        """
+        ),
         #model = LitellmModel(model=Model, api_key=gemini_key),
-        #tools = [route_query]
+        #tools = [route_query],
          handoffs=[
             handoff(linkedin_agent, on_handoff=lambda ctx: on_handoff(linkedin_agent, ctx)),
             handoff(whatsapp_agent, on_handoff=lambda ctx: on_handoff(whatsapp_agent, ctx))
