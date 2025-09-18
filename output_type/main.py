9cfe6e3fcbd3b9f1a32feebc043a3df2136a5047
Moh-Tayyab <m.tayyab1263@gmail.com>
2025-09-18 18:23:47 +0500

    output_type update

--- output_type/main.py
+++ output_type/main.py
@@ -1,6 +1,55 @@
-def main():
-    print("Hello from output-type!")
+#output guard
+from pydantic import BaseModel, Field
+from dotenv import load_dotenv
+import asyncio, os
+from agents import (
+    Agent,
+    Runner,
+    AsyncOpenAI,
+    OpenAIChatCompletionsModel,
+    function_tool,
+    set_tracing_disabled
+)
+from agents.run import RunConfig
+import rich
+
+load_dotenv()
 
 
+async def main():
+    API_KEY=os.environ.get("GEMINI_API_KEY")
+    OPENAI_API_KEY=os.environ.get("OPENAI_API_KEY")
+
+    model=OpenAIChatCompletionsModel(
+        model="gemini-1.5-flash",
+        openai_client=AsyncOpenAI(
+            api_key=API_KEY,
+            base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+        )
+        )
+
+    config=RunConfig(
+        model=model
+        )
+    class WeatherReport(BaseModel):
+            location: str
+            temperature: float
+            condition: str
+            
+    agent = Agent(  
+            name=" WeatherAgent",
+            instructions="You are a expert AI Agent.",
+            output_type=WeatherReport
+        )
+        
+
+    query=input("user query: ")
+    result = await Runner.run(
+            starting_agent = agent,
+            input= query,
+            run_config = config
+            )
+    print(result.final_output)
+ 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
